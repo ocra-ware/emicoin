2d77708070091be73ff048319d7aa6288e4123d7
Ocraware <112951294+ocra-ware@users.noreply.github.com>
2025-05-08 10:35:42 +0200

    Add files via upload

--- contracts/EmicoinPolygon.sol
+++ contracts/EmicoinPolygon.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Emicoin is ERC20, Ownable {
+contract EmicoinPolygon is ERC20, Ownable {
     constructor() ERC20("Emicoin", "EMI") {
         _mint(msg.sender, 500000 * 10 ** decimals());
     }
